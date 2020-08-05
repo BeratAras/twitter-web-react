52a82b5efcd6f0fa443a5318ae915e40bae03d64
Berat Aras <m.berataras@gmail.com>
2020-08-05 14:41:23 +0300

    create

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Button from '../components/button'
+
+function HomePage() {
+  return (
+    <div>
+      <Button>hi</Button>
+    </div>
+  )
+}
+
+export default HomePage
